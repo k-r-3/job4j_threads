e70133d754c01b56363faf4629aa912cf1e98f2e
k-r-3 <rke19922802@gmail.com>
2021-07-06 15:50:03 +0500

    4. Слияние веток [#125030]

--- src/main/java/ru/job4j/Count.java
+++ src/main/java/ru/job4j/Count.java
@@ -3,6 +3,7 @@ package ru.job4j;
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
 
+
 @ThreadSafe
 public class Count {
     @GuardedBy("this")
@@ -15,4 +16,5 @@ public class Count {
     public synchronized int get() {
         return this.value;
     }
+
 }
